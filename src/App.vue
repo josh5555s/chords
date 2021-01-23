cefec96dc3b34552fcea9e4bd20503cb67325594
BuildTools <unconfigured@null.spigotmc.org>
2021-01-23 07:50:50 -0800

    updated inversions symbol, time control, titles

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
     <div id="controls">
-      <button v-on:click="tempoDown">-</button>
-      <input type="text" v-model="tempo">
-      <button v-on:click="tempoUp">+</button>
-      <button @click="bassWander">{{ bass }}</button>
+      <div class="control">
+        <h3>Seconds</h3>
+        <button v-on:click="tempoDown">-</button>
+        <input type="text" v-model="tempo">
+        <button v-on:click="tempoUp">+</button>
+      </div>
+      <div class="control">
+       <h3>Bass</h3>
+       <button @click="bassWander">{{ bassButton }}</button>
+      </div>
+      <div class="control">
+       <h3>Inversions</h3>
+       <button @click="toggleInversions">{{ inversionButton ? 'On' : 'Off' }}</button>
+      </div>
     </div>
     <div id="display">
-    <h1 id="chord">{{ chord }}</h1>
-    <h1 id="inversion">{{ inversion }}</h1>
+    <h1>{{ chord }}<span id="inversion" v-html="inversion"></span>{{ bass }}</h1>
   </div>
 </template>
 
@@ -15,11 +24,13 @@
 export default {
   data() {
     return {
-      tempo: 100,
+      tempo: 2,
       chord: 'Chord',
-      inversion: 'Inversion',
+      inversion: '',
+      bass: '',
+      inversionButton: false,
+      bassButton: 'Root',
       bassCounter: 0,
-      bass: 'Bass: Root'
     }
   },
   methods: {
@@ -28,85 +39,96 @@ export default {
     bassWander() {
       this.bassCounter++
       if (this.bassCounter === 3) {this.bassCounter = 0;}
-      if (this.bassCounter === 0) {this.bass = 'Bass: Root'; }
-      else if (this.bassCounter === 1) {this.bass = 'Bass: Triad';}
-      else if (this.bassCounter === 2) {this.bass = 'Bass: Complex';}
+      if (this.bassCounter === 0) {
+        this.bassButton = 'Root';
+        this.bass = '';
+      }
+      else if (this.bassCounter === 1) {this.bassButton = 'Triad';}
+      else if (this.bassCounter === 2) {this.bassButton = 'Complex';}
+    },
+    toggleInversions() {
+      this.inversionButton = !this.inversionButton;
+      if (!this.inversionButton) {this.inversion = ''}
     },
     newChord() {
-      function getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
-      }
-      let chord = getRandomInt(6)
-      if (chord === 0) { chord = "I" }
-      else if (chord === 1) { chord = "ii" }
-      else if (chord === 2) { chord = "iii" }
-      else if (chord === 3) { chord = "IV" }
-      else if (chord === 4) { chord = "V" }
-      else if (chord === 5) { chord = "vi" }
+        function getRandomInt(max) {
+          return Math.floor(Math.random() * Math.floor(max));
+        }
+        let chord = getRandomInt(6)
+        if (chord === 0) { chord = "I" }
+        else if (chord === 1) { chord = "ii" }
+        else if (chord === 2) { chord = "iii" }
+        else if (chord === 3) { chord = "IV" }
+        else if (chord === 4) { chord = "V" }
+        else if (chord === 5) { chord = "vi" }
 
-      let inversion = getRandomInt(3)
-      if (inversion === 0) { inversion = "Root" }
-      else if (inversion === 1) { inversion = "1st" }
-      else if (inversion === 2) { inversion = "2nd" }
+        let inversion = getRandomInt(3)
+        if (inversion === 0) { inversion = "" }
+        else if (inversion === 1) { inversion = "6".sup() }
+        else if (inversion === 2) { inversion = "6".sup() + "4".sub() }
 
-      let bass;
-      if (this.bassCounter === 2) {
-        bass = getRandomInt(6)
-        if (bass === 0) { bass = "I" }
-        else if (bass === 1) { bass = "ii" }
-        else if (bass === 2) { bass = "iii" }
-        else if (bass === 3) { bass = "IV" }
-        else if (bass === 4) { bass = "V" }
-        else if (bass === 5) { bass = "vi" }
-      } 
-      else if (this.bassCounter === 1) {
-        bass = getRandomInt(2)
-        if (chord === "I") {
+        let bass;
+        if (this.bassCounter === 2) {
+          bass = getRandomInt(6)
           if (bass === 0) { bass = "I" }
-          else if (bass === 1) { bass = "iii" }
-          else if (bass === 2) { bass = "V" }
-        } 
-        else if (chord === "ii") {
-           if (bass === 0) { bass = "ii" }
-          else if (bass === 1) { bass = "IV" }
-          else if (bass === 2) { bass = "vi" }
-        }
-        else if (chord === "iii") {
-           if (bass === 0) { bass = "iii" }
-          else if (bass === 1) { bass = "V" }
-          else if (bass === 2) { bass = "vii" }
-        }
-        else if (chord === "IV") {
-           if (bass === 0) { bass = "IV" }
-          else if (bass === 1) { bass = "vi" }
-          else if (bass === 2) { bass = "I" }
-        }
-        else if (chord === "V") {
-           if (bass === 0) { bass = "V" }
-          else if (bass === 1) { bass = "vii" }
-          else if (bass === 2) { bass = "ii" }
-        }
-        else if (chord === "vi") {
-           if (bass === 0) { bass = "vi" }
-          else if (bass === 1) { bass = "I" }
+          else if (bass === 1) { bass = "ii" }
           else if (bass === 2) { bass = "iii" }
+          else if (bass === 3) { bass = "IV" }
+          else if (bass === 4) { bass = "V" }
+          else if (bass === 5) { bass = "vi" }
+        } 
+        else if (this.bassCounter === 1) {
+          bass = getRandomInt(2)
+          if (chord === "I") {
+            if (bass === 0) { bass = "I" }
+            else if (bass === 1) { bass = "iii" }
+            else if (bass === 2) { bass = "V" }
+          } 
+          else if (chord === "ii") {
+            if (bass === 0) { bass = "ii" }
+            else if (bass === 1) { bass = "IV" }
+            else if (bass === 2) { bass = "vi" }
+          }
+          else if (chord === "iii") {
+            if (bass === 0) { bass = "iii" }
+            else if (bass === 1) { bass = "V" }
+            else if (bass === 2) { bass = "vii" }
+          }
+          else if (chord === "IV") {
+            if (bass === 0) { bass = "IV" }
+            else if (bass === 1) { bass = "vi" }
+            else if (bass === 2) { bass = "I" }
+          }
+          else if (chord === "V") {
+            if (bass === 0) { bass = "V" }
+            else if (bass === 1) { bass = "vii" }
+            else if (bass === 2) { bass = "ii" }
+          }
+          else if (chord === "vi") {
+            if (bass === 0) { bass = "vi" }
+            else if (bass === 1) { bass = "I" }
+            else if (bass === 2) { bass = "iii" }
+          }
         }
-      }
-      else if (this.bassCounter === 0) {
-        bass = '';
-      }
-
-      if (bass !== chord && bass !== '') { chord = `${chord}/${bass}` }
-
-      this.chord = chord;
-      this.inversion = inversion; 
-    }
-  },
-  mounted() { 
-    setInterval(() => {
+        else if (this.bassCounter === 0) {
+          bass = '';
+        }
+        if (bass !== '' && bass !== chord) {this.bass = `/${bass}`}
+        if (this.inversionButton) { this.inversion = inversion }
+        this.chord = chord;
+        this.sleep();
+      },
+      async sleep() {
+        function sleep(ms) {
+          return new Promise(resolve => setTimeout(resolve, ms));
+        }
+        await sleep(this.tempo * 1000)
         this.newChord();
-      }, 5000)
-  },
+      },
+    },
+    mounted() { 
+      this.newChord();
+    },
 }
 
 </script>
@@ -118,12 +140,34 @@ export default {
       font-family: 'Courier New', Courier, monospace;
     }
 
+    #controls {
+      display: flex;
+      justify-content: center;
+    }
+
+    .control {
+      margin: 10px;
+    }
+
+    .control h3 {
+      margin: 2px;
+    }
+
     div {
       margin-top: 50px;
       text-align: center;
     }
 
+    input {
+      width: 20px;
+    }
+
     h1 {
       margin: 0px;
+      font-size: 80px;
+    }
+
+    #inversion {
+      font-size: 60px;
     }
 </style>
